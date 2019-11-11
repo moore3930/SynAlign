ab2cb4bd08f8624835cd15a90b64da7b73a90abb
moore <inbath@163.com>
2019-11-11 17:42:22 +0800

    update

--- SynAlignSent.py
+++ SynAlignSent.py
@@ -414,10 +414,10 @@ class SynAlign(Model):
                         wd_id = t_sent[s][j]
                         if wd_id <= 0:
                             continue
-                        print(wd_id)
-                        print(st_align_score[s][i][j])
-                        print(self.h_exp_dict[wd_id])
-                        print(self.h_var_dict[wd_id])
+                        # print(wd_id)
+                        # print(st_align_score[s][i][j])
+                        # print(self.h_exp_dict[wd_id])
+                        # print(self.h_var_dict[wd_id])
                         if st_align_score[s][i][j] > self.h_exp_dict[wd_id] + self.h_var_dict[wd_id]:
                             fs_multi_wa_out.write('num-' + str(sent_num) + ' ' + str(i + 1) + ' -> ' + str(j + 1) + '\n')
 
