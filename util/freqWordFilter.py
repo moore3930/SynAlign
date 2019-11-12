99eed04348aa4eac218d61c22b123d5dce79546d
moore <inbath@163.com>
2019-11-12 20:54:52 +0800

    debug

--- util/freqWordFilter.py
+++ util/freqWordFilter.py
@@ -19,8 +19,12 @@ def main():
     freq_set = set([item[0] for item in w_cnt.most_common(int(mc))])
     fout = open(output_file, 'w')
     for line in open(input_file):
-        line_array = repalce(line.strip().split(' '), freq_set, 'UNK')
-        fout.write(' '.join(line_array) + '\n')
+        lst_tuple = []
+        for sent in line.strip().split('\t'):
+            line_array = repalce(sent.strip().split(' '), freq_set, 'UNK')
+            lst_tuple.append(line_array)
+        out_lst = [' '.join(line_array) for line_array in lst_tuple]
+        fout.write('\t'.join(out_lst) + '\n')
     fout.flush()
     fout.close()
 
