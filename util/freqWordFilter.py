b31466d5c457aa3393d5ca31de95c11b3bde067c
moore <inbath@163.com>
2019-11-12 20:42:35 +0800

    update

---
+++ util/freqWordFilter.py
@@ -0,0 +1,28 @@
+import sys
+from collections import *
+
+def repalce(lst, frq_set, repalce_word):
+    for i in range(len(lst)):
+        if lst[i] not in frq_set:
+            lst[i] = repalce_word
+    return lst
+
+def main():
+    input_file = sys.argv[1]
+    output_file = sys.argv[1] + '.fmt'
+    mc = sys.argv[2]
+    w_cnt = Counter()
+    for line in open(input_file):
+        line_array = line.strip().split(' ')
+        for w in line_array:
+            w_cnt.update({w: 1})
+    freq_set = set([item[0] for item in w_cnt.most_common(int(mc))])
+    fout = open(output_file, 'w')
+    for line in open(input_file):
+        line_array = repalce(line.strip().split(' '), freq_set, 'UNK')
+        fout.write(' '.join(line_array) + '\n')
+    fout.flush()
+    fout.close()
+
+if __name__ == '__main__':
+    main()
