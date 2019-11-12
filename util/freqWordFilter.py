76e4aec0ace4f4cb071b61ed36db6a2418e03bc4
moore <inbath@163.com>
2019-11-12 21:06:21 +0800

    update

--- util/freqWordFilter.py
+++ util/freqWordFilter.py
@@ -10,21 +10,29 @@ def repalce(lst, frq_set, repalce_word):
 def main():
     input_file = sys.argv[1]
     output_file = sys.argv[1] + '.fmt'
-    mc = sys.argv[2]
-    w_cnt = Counter()
+    s_mc = sys.argv[2]
+    t_mc = sys.argv[3]
+    sw_cnt = Counter()
+    tw_cnt = Counter()
     for line in open(input_file):
-        line_array = line.strip().split(' ')
-        for w in line_array:
-            w_cnt.update({w: 1})
-    freq_set = set([item[0] for item in w_cnt.most_common(int(mc))])
+        line_array = line.strip().split('\t')
+        if len(line_array) != 2:
+            print(line_array)
+            print('Wrong! Musk be source concat target with tab')
+            return
+        for w in line_array[0].split(' '):
+            sw_cnt.update({w: 1})
+        for w in line_array[1].split(' '):
+            tw_cnt.update({w: 1})
+    s_freq_set = set([item[0] for item in sw_cnt.most_common(int(s_mc))])
+    t_freq_set = set([item[0] for item in tw_cnt.most_common(int(t_mc))])
+
     fout = open(output_file, 'w')
     for line in open(input_file):
-        lst_tuple = []
-        for sent in line.strip().split('\t'):
-            line_array = repalce(sent.strip().split(' '), freq_set, 'UNK')
-            lst_tuple.append(line_array)
-        out_lst = [' '.join(line_array) for line_array in lst_tuple]
-        fout.write('\t'.join(out_lst) + '\n')
+        line_array = line.strip().split('\t')
+        s_sent_array = repalce(line_array[0].strip().split(' '), s_freq_set, 'UNK')
+        t_sent_array = repalce(line_array[1].strip().split(' '), t_freq_set, 'UNK')
+        fout.write(' '.join(s_sent_array) + '\t' + ' '.join(t_sent_array) + '\n')
     fout.flush()
     fout.close()
 
