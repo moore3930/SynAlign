e4043aad71c38e4409494b59e4eb2ddec1deb258
moore <inbath@163.com>
2019-11-14 22:04:07 +0800

    update

--- SynAlignSent.py
+++ SynAlignSent.py
@@ -133,8 +133,8 @@ class SynAlign(Model):
         target_sent_embed = tf.nn.embedding_lookup(self.target_emb_table, target_sent)  # [?, m, 128]
 
         # pooling
-        source_sent_embed = tf.layers.average_pooling1d(source_sent_embed, 3, 1, padding='SAME')
-        target_sent_embed = tf.layers.average_pooling1d(target_sent_embed, 3, 1, padding='SAME')
+        source_sent_embed = tf.layers.average_pooling1d(source_sent_embed, 3, 1, padding='SAME') + source_sent_embed
+        target_sent_embed = tf.layers.average_pooling1d(target_sent_embed, 3, 1, padding='SAME') + target_sent_embed
 
         source_mask_tile = tf.tile(tf.expand_dims(source_mask, 2), [1, 1, tf.shape(target_mask)[1]])    # [?, n, m]
         target_mask_tile = tf.tile(tf.expand_dims(target_mask, 1), [1, tf.shape(source_mask)[1], 1])    # [?, n, m]
