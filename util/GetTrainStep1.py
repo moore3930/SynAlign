c0d44ecb85f210a63c57f1615a1757c2442efded
moore <inbath@163.com>
2019-11-14 10:45:18 +0800

    update

--- util/GetTrainStep1.py
+++ util/GetTrainStep1.py
@@ -7,13 +7,15 @@ def main():
     input_file_3 = sys.argv[3]
     sent_set_1 = set()
     sent_set_2 = set()
-    fout1 = open(input_file_1 + '.s', 'w')
-    fout2 = open(input_file_1 + '.t', 'w')
-    for line in open(input_file_2):
-        sent_set_1.add(line.strip())
-    for line in open(input_file_3):
-        sent_set_2.add(line.strip())
+    fout1 = open(input_file_3 + '.s', 'w')
+    fout2 = open(input_file_3 + '.t', 'w')
     for line in open(input_file_1):
+        sent_set_1.add(line.strip())
+    print(len(sent_set_1))
+    for line in open(input_file_2):
+        sent_set_2.add(line.strip())
+    print(len(sent_set_2))
+    for line in open(input_file_3):
         line_array = line.strip().strip('\t')
         if line_array[0] in sent_set_1 and line_array[1] in sent_set_2:
             fout1.write(line_array[0] + '\n')
