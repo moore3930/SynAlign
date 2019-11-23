ce6d1c69952e5eb893eec54eccb8d936da1559df
moore <inbath@163.com>
2019-11-23 16:18:09 +0800

    update

--- test-debug.sh
+++ test-debug.sh
@@ -1,1 +1,5 @@
-python SynAlignSent.py -train_data='data/ro-en/ro-en-train.txt' -eval_data='data/ro-en/ro-en-test.txt' -eval_data_wa='data/ro-en/ro-en-test-wa.txt'
+BATCH=24
+NEG_NUM=16
+LR=0.00075
+ALPHA=1.0
+CUDA_VISIBLE_DEVICES=0 python SynAlignSent.py -epoch=30 -embed_dim=256 -result_path='data/ro-en.gd' -train_data='data/ro-en/ro-en-merge-2.txt' -eval_data='data/ro-en/ro-en-test.txt' -eval_data_wa='data/ro-en/ro-en-test-wa.txt' -output_prefix='data/ro-en' -num_neg=${NEG_NUM} -lr=${LR} -alpha=${ALPHA} -batch=${BATCH}
